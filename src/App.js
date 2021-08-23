2cf2725324cfb9cf0d7a6840f1ff22f25791d1fa
Allexandar93 <zivoticaleksandar00@gmail.com>
2021-08-24 00:19:47 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import './App.scss';
+import marked from 'marked'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+const placeholder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
+
+class App extends Component{
+  state = {
+    text: placeholder
+  }
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+
+  }
+  
+  
+
+
+  render() {
+    const {text} = this.state
+    const markdown = marked(text, {breaks: true})
+    
+    return(
+      <header>
+      <div>
+        <h1 className="text-center m-4">Convert your Markdown</h1>
+        <div className="row" id="rows">
+          
+          <div className="col-6">
+          <h5>Enter your markdown</h5>
+            <textarea className="form-control p-2" id="editor" value={text} onChange={this.handleChange} />
+          </div>
+
+          <div className="col-6" >
+            <h5 className="text-center">See the result:</h5>
+            <div className="preview" id="preview" dangerouslySetInnerHTML={{__html: markdown}} />
+          </div>
+
+        </div>
+      </div>
       </header>
-    </div>
-  );
+
+    )
+
+  }
+
 }
 
 export default App;
